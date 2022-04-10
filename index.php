49eb259ad57712e3899076faea39d86b62850889
Zaidkhan <63402453+zaidkhan694@users.noreply.github.com>
2022-04-10 21:54:53 +0530

    Name change

--- index.php
+++ index.php
@@ -118,7 +118,7 @@
   <div class="card">
   <img src="images/img2.jpg.jpg" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">Explore Space</h5>
+    <h5 class="card-title">Explore More With us</h5>
     <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
     <a href="#" class="btn btn-success">Explore</a>
   </div>
