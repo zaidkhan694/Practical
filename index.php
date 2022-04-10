6d58297a2d9158c403394760bed1171761bd1812
Zaidkhan <63402453+zaidkhan694@users.noreply.github.com>
2022-04-10 21:56:07 +0530

    Footer change

--- index.php
+++ index.php
@@ -203,7 +203,7 @@
 </section>
 
 <footer class="my-5">
-    <p class="text-center p-3 bg-dark text-white ">@Z.K.Production</p>
+    <p class="text-center p-3 bg-dark text-white ">@Z.K.Production Website</p>
 </footer>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>  
